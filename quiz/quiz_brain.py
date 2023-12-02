7d1ffcb364998d2524debf44891770fee090a599
Shankhanil Ghosh <53429536+shan19990@users.noreply.github.com>
2023-12-02 19:38:10 +0530

    Add files via upload

--- quiz/quiz_brain.py
+++ quiz/quiz_brain.py
@@ -1,15 +1,19 @@
+import random
+
 class QuizBrain:
     
     def __init__(self,question_bank):
         self.question_number = 0
+        self.choose_number = random.randint(1,50)
         self.question_list = question_bank
         self.score = 0
         
     def next_question(self):
-        current_question = self.question_list[self.question_number]
+        current_question = self.question_list[self.choose_number]
         self.question_number += 1
         choice = input(f"Q.{self.question_number} {current_question.text}: True or False? ")
         self.check_answer(choice,current_question)
+        self.choose_number = random.randint(1,50)
         
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
@@ -30,3 +34,4 @@ class QuizBrain:
         
     def final_score(self):
         print(f"Your Final Score is : {self.score}")
+        
